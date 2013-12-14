d14dd7c466ebdd485856535f8301bac901f88779
Charles Johnson <charles@opscode.com>
2013-12-14 11:49:36 -0800

    Updated spec_helper to fix Growl notifications when running rspec via Gaurd

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,17 @@ RSpec.configure do |c|
   c.version   = '12.04'
   c.log_level = :warn
 end
+
+# Required for Growl notifications to work properly
+def gem_available?(name)
+  Gem::Specification.find_by_name(name)
+rescue Gem::LoadError
+  false
+rescue
+  Gem.available?(name)
+end
+
+if gem_available?('safe_yaml')
+  SafeYAML::OPTIONS[:deserialize_symbols] = true
+  SafeYAML::OPTIONS[:default_mode] = 'unsafe'
+end
